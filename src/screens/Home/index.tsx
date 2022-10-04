28329e1974e9a04ab4f3a5d251003e2b2aa94f8d
Yago Afonso <yago.afonso@hotmail.com>
2022-10-04 14:54:02 -0400

    add input text

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  );
+}
